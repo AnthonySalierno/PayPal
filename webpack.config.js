89843067bc53eb661a363c4b2737463b8f5aad9f
Anthony Salierno <anthony.salierno@gmail.com>
2016-07-21 16:47:41 -0700

    Initialize Node server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
