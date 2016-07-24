08ab28b00bc8b8b6898d514fb3fe648b54af2192
Anthony Salierno <anthony.salierno@gmail.com>
2016-07-23 19:01:51 -0700

    Update to ES6

--- database.js
+++ database.js
@@ -1,19 +1,14 @@
 import Sequelize from 'sequelize';
 import path from 'path';
 
-const sequelize = new Sequelize('PayPal', null, null, {
+export const sequelize = new Sequelize('PayPal', null, null, {
   dialect: 'sqlite',
   storage: path.join(__dirname, 'paypal.sqlite')
 });
 
-const Payments = sequelize.define('Payments', {
+export const Payments = sequelize.define('Payments', {
   email: Sequelize.STRING,
   amount: Sequelize.INTEGER,
   currency: Sequelize.STRING,
   category: Sequelize.STRING,
 });
-
-module.exports = {
-  Payments,
-  sequelize,
-};
