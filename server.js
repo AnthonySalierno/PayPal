82e29c0a8fff392cbff0a3cc581554d3a97a8d63
Anthony Salierno <anthony.salierno@gmail.com>
2016-07-23 19:03:44 -0700

    Allow user to POST payments

--- server.js
+++ server.js
@@ -3,15 +3,33 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import bodyParser from 'body-parser';
+
 import config from './webpack.config.js';
 import initializeDatabase from './dummyData';
+import { addPayment } from './controller';
 
 const app = express();
 const compiler = webpack(config);
 
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, '/dist')));
 app.use(webpackMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
+app.post('/send-money', (req, res) => {
+  addPayment(req.body)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
+});
+app.get('/transaction-history', (req, res) => {
+
+});
 app.get('*', function response(req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
--- src/Components/SendMoneyView.js
+++ src/Components/SendMoneyView.js
@@ -1,38 +1,99 @@
 import React from 'react';
+//import 'whatwg-fetch';
 
 class SendMoneyView extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
+    this.default = {
+      email: '',
+      amount: '0.00',
+      message: '',
+      category: 'Personal',
       moneySent: false,
       currency: 'USD',
     };
+
+    this.state = this.default;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.clearForm = this.clearForm.bind(this);
+    this.submitPayment = this.submitPayment.bind(this);
+  }
+
+  handleChange(stateProp, event) {
+    const newState = {};
+    newState[stateProp] = event.target.value;
+    this.setState(newState);
+  }
+
+  clearForm() {
+    this.setState(this.default);
+  }
+
+  submitPayment() {
+    const {email, amount, category, currency} = this.state
+    fetch('/send-money', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        amount,
+        category,
+        currency,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => console.log(res))
+    this.clearForm();
   }
 
   render() {
     return (
       <div>
         <h1>Send Money</h1>
-        <form>
           <div>
-            To:<input className="email-input" type="text" name="email-address"/>
+            To:
+              <input
+                className="email-input"
+                type="text"
+                name="email-address"
+                value={this.state.email}
+                onChange={(e) => this.handleChange('email', e) }
+              />
           </div>
           <div className="currency-input">
-            $<input type="text" name="currency"/>
-            <select>
+            $
+            <input
+              type="text"
+              name="currency"
+              value={this.state.amount}
+              onChange={e => this.handleChange('amount', e) }
+            />
+            <select onChange={e => this.handleChange('currency', e) }>
               <option value="usd">USD</option>
               <option value="eur">EUR</option>
               <option value="jpy">JPY</option>
             </select>
           </div>
           <div>
-            Message (optional):<input className="email-input" type="text" name="emailaddress"/>
+            Message (optional):
+            <input
+              className="email-input"
+              type="text"
+              name="emailaddress"
+              value={this.state.message}
+              onChange={(e) => this.handleChange('message', e) }
+            />
           </div>
           <div>
             <input type="radio" name="send-to" value="person"/>I'm sending money to family or friends<br/>
             <input type="radio" name="send-to" value="business"/>I'm paying for goods or services<br/>
           </div>
-        </form>
+          <br/>
+          <br/>
+          <br/>
+          <button onClick={this.clearForm}>Clear</button>
+          <button onClick={this.submitPayment}>Next</button>
       </div>
     )
   }
