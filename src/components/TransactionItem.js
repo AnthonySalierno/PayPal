56b41edaa2f09947079e81c922a85c08e98f67c2
Anthony Salierno <anthony.salierno@gmail.com>
2016-07-24 23:31:47 -0700

    Format dates in TransactionHistoryView

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,9 +1,11 @@
 export default function TransactionItem({amount, email, currency, createdAt}) {
+  const date = new Date(createdAt);
+  const formattedDate = date.toLocaleDateString('en-us');
   return (
     <div className="transaction-record">
-      <span className="transaction-record payment">{amount + " " + currency}</span>
+      <span className="transaction-record payment">{`${amount} ${currency}`}</span>
       <span className="transaction-record email">{email}</span>
-      <span className="transaction-record created-at">{createdAt}</span>
+      <span className="transaction-record created-at">{formattedDate}</span>
     </div>
   )
 }
